6995b46769e540cab82e20178148c81bacaf34f6
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-08-19 21:52:41 -0500

    update package mgmt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements=['invoke', 'boto3', 'pyyaml', 'requests', 'colorama']
+requirements=['invoke', 'boto3', 'pyyaml', 'requests', 'colorama', 'pkg_resources']
 
 setup(
     name="secretctl",

c61f67d2a3bec11e9feff21b1f391f8bc9fbb370
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-08-19 20:52:44 -0500

    local pylintrc

--- secretctl/main.py
+++ secretctl/main.py
@@ -1,6 +1,7 @@
 """main.py"""
 from invoke import Collection, Program
-from setuptools_scm import get_version
+import pkg_resources
 from secretctl import cli
 
-program = Program(namespace=Collection.from_module(cli), version=get_version(root='..', relative_to=__file__))
+#program = Program(namespace=Collection.from_module(cli), version=get_version(root='..', relative_to=__file__))
+program = Program(namespace=Collection.from_module(cli), version=pkg_resources.get_distribution('secretctl').version)

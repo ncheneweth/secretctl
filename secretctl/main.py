869bbbb124de01fa541442945b0d0b4567d7e816
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-09-04 19:58:06 -0700

    pipeline iteration

--- secretctl/main.py
+++ secretctl/main.py
@@ -3,5 +3,5 @@ from invoke import Collection, Program
 from better_setuptools_git_version import get_version
 from secretctl import cli
 
-program = Program(namespace=Collection.from_module(cli), version=get_version().replace('+dirty',''))
+program = Program(namespace=Collection.from_module(cli), version=get_version().replace('+dirty', ''))
 #program = Program(namespace=Collection.from_module(cli), version='0.0.15')
